cc85112d50b6f7a725f6e6dc5d2cfcac4f44890b
Bjarke Rasmus Nicolaisen <bjarkerasmusnicolaisen@Bjarkes-MBP.home>
2023-09-13 20:36:35 +0200

    did some work on monday exercises CompSys

--- bjarkoff/CompSys/Uge3/exercises/mandag/int2ascii.c
+++ bjarkoff/CompSys/Uge3/exercises/mandag/int2ascii.c
@@ -3,8 +3,12 @@
 #include <assert.h>
 
 int read_int(FILE* f, int *out) {
-  
+  return (fread(out, sizeof(int), 1, f) + 1) % 2; // returns 0 when fread == 1, and 1 when fread == 0.
 }
+// Important note to self: Originallt above I wrote "fread(&out,...)", but since the input
+// is already int *out, that means (I think) that it is the pointer to out that is given,
+// not the int out itself. Therefore no need to go to pointer via &out, since out is already
+// the pointer.
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -12,10 +16,10 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
   FILE *f = fopen(argv[1], "r");
+  int x;
   assert(f != NULL);
-  char c;
-  while (fread(&c, sizeof(char), 1, f) == 1) {
-  printf("0x%.2x ", (int)c);
+  while (read_int(f, &x) == 0) {
+  printf("%d", (int)x);
   printf("\n");
   }
   return EXIT_SUCCESS;
