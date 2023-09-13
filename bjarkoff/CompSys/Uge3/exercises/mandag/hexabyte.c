b4ba202b78c9a553f449a3cb9dbbc8b287b060f7
Bjarke Rasmus Nicolaisen <bjarkerasmusnicolaisen@Bjarkes-MBP.home>
2023-09-13 15:20:29 +0200

    uge 3 så småt tilføjet

---
+++ bjarkoff/CompSys/Uge3/exercises/mandag/hexabyte.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Bad value of input\n");
+    return EXIT_FAILURE;
+  }
+  FILE *f = fopen(argv[1], "r");
+  assert(f != NULL);
+  char c;
+  while (fread(&c, sizeof(char), 1, f) == 1) {
+  printf("0x%.2x ", (int)c);
+  printf("\n");
+  }
+  return EXIT_SUCCESS;
+}
